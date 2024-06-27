bec6007aef170189d932a4c52ca0543f57332f5e
gogomezc <go.gomezc@duocuc.cl>
2024-06-26 23:54:53 -0400

    vistas agregadas, ahora creare la base de datos

--- ecomerce/ecomerce/settings.py
+++ ecomerce/ecomerce/settings.py
@@ -1,9 +1,12 @@
 
 
 from pathlib import Path
+import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 
@@ -24,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -103,6 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
